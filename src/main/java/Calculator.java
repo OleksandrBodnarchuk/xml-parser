8b67b3666328b1a10543a7607a68c79fa6897b05
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2022-02-01 15:08:21 +0100

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+/* TODO:
+- Należy napisać parser pliku z kursami walut (dopuszczalne jest użycie bibliotek do parsowania XMLa) i klasę kalkulatora.
+- Kalkulator powinien przyjmować kwotę w EUR i docelową walutę, zwracać kwotę w docelowej walucie.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+}
